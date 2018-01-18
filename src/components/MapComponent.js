311d5958de91a5d319f44a0e7fabf534ccb31a11
Aravind Balla <saiaravind.b@accoliteindia.com>
2018-01-18 17:19:25 +0530

    Add Prettier and ESLint

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,29 +1,26 @@
-import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
+import React, { Component } from 'react'
+import GoogleMapReact from 'google-map-react'
 
-import MyGreatPlace from './MyGreatPlace';
+import MyGreatPlace from './MyGreatPlace'
+import { indiaLatLong } from '../util/constants'
 
 class MapComponent extends Component {
   static defaultProps = {
-    center: {lat: 21.14, lng: 79.09},
-    zoom: 6
-  };
+    center: indiaLatLong,
+    zoom: 6,
+  }
 
   render() {
     return (
       <GoogleMapReact
         defaultCenter={this.props.center}
         defaultZoom={this.props.zoom}
-        style={{height: 100 + 'vh'}}
+        style={{ height: 100 + 'vh' }}
       >
-        <MyGreatPlace
-          lat={17.38}
-          lng={78.48}
-          text={'Hyderabad'}
-        />
+        <MyGreatPlace lat={17.38} lng={78.48} text={'Hyderabad'} />
       </GoogleMapReact>
-    );
+    )
   }
 }
 
-export default MapComponent;
+export default MapComponent
---
+++ src/util/constants.js
@@ -0,0 +1,1 @@
+export const indiaLatLong = { lat: 21.14, lng: 79.09 }
